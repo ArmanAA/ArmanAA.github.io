147b0383c55e82c062a22b4aacc65c3633a58840
Arman Akhbari <akhbari@usc.edu>
2018-11-04 13:58:54 +0900

    idk

--- js/contact_me.js
+++ js/contact_me.js
@@ -22,7 +22,7 @@ $(function() {
       // $this = $("#sendMessageButton");
       // $this.prop("disabled", false); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "./mail/contact_me.php",
+        url: "././mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
